873ded0ef6c60c5851cc432d1adfb5f347e2c8a7
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2025-11-09 16:16:24 +0530

    Fixed Swagger-UI

--- movie-service/src/main/java/com/movietime/movie_service/config/OpenApiConfig.java
+++ movie-service/src/main/java/com/movietime/movie_service/config/OpenApiConfig.java
@@ -1,8 +1,19 @@
 package com.movietime.movie_service.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-    // Swagger auto-scans; access via http://localhost:8081/swagger-ui.html
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("🎬 Movie Service API")
+                        .version("1.0")
+                        .description("Endpoints for managing movies, shows, and seat availability"));
+    }
 }
